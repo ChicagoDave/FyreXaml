212d2c96a35bc8848ec511b01da8873027f8a3d1
ChicagoDave <david.cornelson@gmail.com>
2014-03-26 00:10:30 -0500

    WinDispatcher.

---
+++ fyrevm/PortableDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FyreVM
+{
+    public abstract class PortableDispatcher
+    {
+        public abstract void Invoke(Action action);
+    }
+}
